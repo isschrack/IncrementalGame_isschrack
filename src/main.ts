8606ce7c2b272937d47c7531db98a27259ef7047
Izzy Schrack <superizzy@outlook.com>
2025-10-06 21:26:31 +0000

    step 2 complete: added counter_click function,  created variable alien_counter, and displayed it in HTML

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+declare global {
+  var counter_click: () => void;
+}
+
+let alien_counter = 0;
+
+function counter_click(): void {
+  alien_counter += 1;
+  document.querySelector("[label='counter']")!.textContent =
+    `Aliens Captured: ${alien_counter}`;
+  console.log(alien_counter);
+}
+globalThis.counter_click = counter_click;
+
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-  <button>👽</button>
+  <div label='counter'>Aliens Captured: ${alien_counter}</div>
+  <button onclick="counter_click()">👽</button>
 `;
-
-
