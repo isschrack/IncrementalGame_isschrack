e545ac82633597100ca1462ac00876b67323f56f
Izzy Schrack <superizzy@outlook.com>
2025-10-06 21:51:56 +0000

    step 3 completed: - added increment for counter_click function and user_click variable so if the user gets upgrades they can change their user_click value - used setInterval function for upgrades that automatically click the cookie for you (also where increment parameter comes in handy) - added some variables for upgrades, will add more code to them later. - a few variables are constants right now because depo lint forced me to do that

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,32 @@ import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 declare global {
-  var counter_click: () => void;
+  var counter_click: (increment: number) => void;
+  var user_click: number;
 }
 
 let alien_counter = 0;
+let user_click = 1;
+//set jets back to 0 when you're done testing
+let jets = 1;
+let tanks = 0;
+let nukes = 0;
 
-function counter_click(): void {
-  alien_counter += 1;
+//increases the alien counter by the increment value
+function counter_click(increment: number): void {
+  alien_counter += increment;
   document.querySelector("[label='counter']")!.textContent =
     `Aliens Captured: ${alien_counter}`;
   console.log(alien_counter);
 }
+
 globalThis.counter_click = counter_click;
+globalThis.user_click = user_click;
+
+let clicker_interval = setInterval(counter_click, 1000, jets, tanks, nukes);
 
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
   <div label='counter'>Aliens Captured: ${alien_counter}</div>
-  <button onclick="counter_click()">👽</button>
+  <button onclick="counter_click(${user_click})">👽</button>
 `;
